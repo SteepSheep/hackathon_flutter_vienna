fbc65a290ff249b02a90d283847bc818dd3f51ff
Stefan <bock.stefan@gmx.at>
2024-08-03 16:12:07 +0200

    added toJson for events

--- lib/game_events/game_event.dart
+++ lib/game_events/game_event.dart
@@ -44,20 +44,37 @@ class Join extends GameEvent {
   const Join(this.name) : super(type: EventType.join);
 
   final String name;
+
+  Map<String, dynamic> toJson() => {
+        'type': type.index,
+        'name': name,
+      };
 }
 
 class StartGame extends GameEvent {
   const StartGame() : super(type: EventType.start);
+
+  Map<String, dynamic> toJson() => {'type': type.index};
 }
 
 class SubmitArtist extends GameEvent {
   const SubmitArtist(this.artist) : super(type: EventType.sendArtist);
 
   final String artist;
+
+  Map<String, dynamic> toJson() => {
+        'type': type.index,
+        'artist': artist,
+      };
 }
 
 class Answer extends GameEvent {
   const Answer(this.answer) : super(type: EventType.sendAnswer);
 
   final int answer;
+
+  Map<String, dynamic> toJson() => {
+        'type': type.index,
+        'answer': answer,
+      };
 }
