0e9a385d9586a7a7d8c254ccad934d3b612033fd
Radosław Lichtarski <rradzzio@gmail.com>
2024-08-03 16:35:57 +0200

    update of change of game state

--- lib/game_data/game_state.dart
+++ lib/game_data/game_state.dart
@@ -5,16 +5,18 @@ part 'question_data.dart';
 class GameState extends Equatable {
   const GameState({
     this.players = const [],
-    this.questions = const [],
-    this.durations = const {},
     this.answers = const {},
+    this.durations = const {},
+    this.currentQuestionIndex = 0,
+    this.questions = const [],
   });
 
   final List<String> players;
   final Map<String, int> answers;
   final Map<String, double> durations;
+  final int currentQuestionIndex;
   final List<QuestionData> questions;
 
   @override
-  List<Object?> get props => [players, answers, durations, questions];
+  List<Object?> get props => [players, answers, durations, currentQuestionIndex, questions];
 }
--- lib/game_data/question_data.dart
+++ lib/game_data/question_data.dart
@@ -3,16 +3,14 @@ part of 'game_state.dart';
 class QuestionData extends Equatable {
   const QuestionData({
     required this.songUrl,
-    required this.question,
     required this.correctAnswer,
     required this.answers,
   });
 
   final String songUrl;
-  final String question;
   final String correctAnswer;
   final List<String> answers;
 
   @override
-  List<Object?> get props => [songUrl, question, correctAnswer, answers];
+  List<Object?> get props => [songUrl, correctAnswer, answers];
 }
