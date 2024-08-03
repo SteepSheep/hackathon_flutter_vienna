c1451a48e2e0f95dd31f5a1e5cec2f32f68b5205
Radosław Lichtarski <rradzzio@gmail.com>
2024-08-03 17:37:00 +0200

    remove the scaffold

--- lib/screens/song_question_screen.dart
+++ lib/screens/song_question_screen.dart
@@ -6,54 +6,45 @@ import 'package:hackathon_flutter_vienna/screens/widgets/answer_widget.dart';
 
 class SongQuestionScreen extends StatefulWidget {
   final QuestionData questionData;
-  const SongQuestionScreen({
-    super.key,
-    required this.questionData
-  });
+  const SongQuestionScreen({super.key, required this.questionData});
 
   @override
   State<SongQuestionScreen> createState() => _SongQuestionScreenState();
 }
 
 class _SongQuestionScreenState extends State<SongQuestionScreen> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            const Text(
-              'What song is this?',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const Icon(Icons.music_note),
-            ValueListenableBuilder(
-              valueListenable: gameLogic,
-              builder: (context, value, child) {
-                if (value.questions.isNotEmpty) {
-
-                  return ListView.builder(
-                    itemCount: value.questions[value.currentQuestionIndex].answers.length,
-                    itemBuilder: (context, index) {
-                      String answer = value.questions[value.currentQuestionIndex].answers[index];
-                      return AnswerWidget(
-                        answer: answer, 
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          const Text(
+            'What song is this?',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          const Icon(Icons.music_note),
+          ValueListenableBuilder(
+            valueListenable: gameLogic,
+            builder: (context, value, child) {
+              if (value.questions.isNotEmpty) {
+                return ListView.builder(
+                  itemCount: value.questions[value.currentQuestionIndex].answers.length,
+                  itemBuilder: (context, index) {
+                    String answer = value.questions[value.currentQuestionIndex].answers[index];
+                    return AnswerWidget(
+                        answer: answer,
                         onSelected: () {
-                          gameLogic.addEvent(Answer(answer: index, name: value));
-                        }
-                      );
-                    },
-                  );
-                }
-                return const SizedBox();
-              },
-            )
-            
-          ],
-        ),
+                          gameLogic.addEvent(Answer(answer: index, name: 'value'));
+                        });
+                  },
+                );
+              }
+              return const SizedBox();
+            },
+          )
+        ],
       ),
     );
   }
